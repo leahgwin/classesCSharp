28e755af10854104eb19d6f277ab2a907616d03c
leahgwin <leahgwin@gmail.com>
2018-08-24 14:27:36 -0500

    made the custom types for employees

---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace classes
+{
+    // this is the custom type for Employee. the employee has the following properties:
+    public class Employee
+    {
+        public string firstName { get; }
+        public string lastName { get; }
+        public string title { get; }
+        public DateTime startDate { get; }
+    }
+
+    // this is the custom type for company. the company has the following properties:
+    public class Company
+    {
+
+        // Some readonly properties (let's talk about gets, baby)
+        public string Name { get; }
+        public DateTime CreatedOn { get; }
+
+        // Create a public property for holding a list of current employees
+
+        /*
+            Create a constructor method that accepts two arguments:
+                1. The name of the company
+                2. The date it was created
+
+            The constructor will set the value of the public properties
+        */
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create an instance of a company. Name it whatever you like.
+
+            // Create three employees
+
+            // Assign the employees to the company
+
+            /*
+                Iterate the company's employee list and generate the
+                simple report shown above
+            */
+        }
+    }
+}
